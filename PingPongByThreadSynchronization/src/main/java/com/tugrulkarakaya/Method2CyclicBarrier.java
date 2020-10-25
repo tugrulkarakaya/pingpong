2ebf77c333ca359ec9034d11052c37098ff995ef
Tugrul Karakaya <tugrul.karakaya@gmail.com>
2020-10-25 05:45:45 +0000

    a Proper Thread interraption mechanism has been implemented/ While iteration is placed into the thread. So no need to re-create threads anymore.

--- PingPongByThreadSynchronization/src/main/java/com/tugrulkarakaya/Method2CyclicBarrier.java
+++ PingPongByThreadSynchronization/src/main/java/com/tugrulkarakaya/Method2CyclicBarrier.java
@@ -15,33 +15,38 @@ public class Method2CyclicBarrier {
 
     public void runPingPong(){
         service = Executors.newFixedThreadPool(2);
-
-        while(true) {
             service.submit(() -> this.printPing(c1, c2));
             service.submit(() -> this.printPong(c1, c2));
-        }
     }
 
 
 
     public void printPing(CyclicBarrier c1, CyclicBarrier c2) {
-        try {
-            c1.await();
-            System.out.println("PING");
-            c2.await();
-        } catch (InterruptedException | BrokenBarrierException e) {
-            e.printStackTrace();
-        }
+        while(!Thread.currentThread().isInterrupted()) {
+            try {
+                c1.await();
+                System.out.println("PING");
+                c2.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            catch(BrokenBarrierException ex){
 
+            }
+        }
     }
 
     public void printPong(CyclicBarrier c1, CyclicBarrier c2){
-        try {
-            c1.await();
-            c2.await();
-        } catch (InterruptedException | BrokenBarrierException e) {
-            //handle here
+        while(!Thread.currentThread().isInterrupted()) {
+            try {
+                c1.await();
+                c2.await();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            } catch(BrokenBarrierException ex){
+
+            }
+            System.out.println("PONG");
         }
-        System.out.println("PONG");
     }
 }
